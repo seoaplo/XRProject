23733f9ec53450a25a5cebc4264eb979e7ca6d91
Beco <ghewqa1234@gmail.com>
2020-01-22 15:45:31 +0900

    Prepare Network

---
+++ Source/XRProject/Public/XRGameInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NetworkManager.h"
+#include "XRGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XRPROJECT_API UXRGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+    virtual void Init() override;
+    virtual void Shutdown() override;
+
+    FORCEINLINE UNetworkManager& GetNetworkManager() {
+        return *NetworkManager;
+    }
+private:
+    UPROPERTY(VisibleAnywhere, Meta = (AllowPrivateAccess = true))
+    UNetworkManager* NetworkManager;
+};
+
+#define GetNetMgr Cast<UXRGameInstance>(GetGameInstance())->GetNetworkManager
