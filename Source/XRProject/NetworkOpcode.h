588643d2283527397836cb79001440f0dffe619a
Sofast <Sofast@DESKTOP-IABQBVJ>
2020-02-04 18:49:00 +0900

    ddd
    ddd

--- Source/XRProject/NetworkOpcode.h
+++ Source/XRProject/NetworkOpcode.h
@@ -3,69 +3,76 @@
 
 enum class ENetworkCSOpcode : uint16_t
 {
-	// LoginServer
-	kCreateAccountRequest = 0,
-	kLoginRequest,
+    // LoginServer
+    kCreateAccountRequest = 0,
+    kLoginRequest,
 
-	// LobbyServer
-	kLobbyConfirmRequest = 100,
-	kCharacterListRequest,
-	kCharacterCreateRequest,
-	kCharacterDeleteRequest,
-	kCharacterSelectionRequest,
+    // LobbyServer
+    kLobbyConfirmRequest = 100,
+    kCharacterListRequest,
+    kCharacterCreateRequest,
+    kCharacterDeleteRequest,
+    kCharacterSelectionRequest,
 
-	// ZoneServer
-	kZoneConrifmRequest = 1000,
-	kNotifyCurrentChrPosition = 1001,
-	kNotifyMonsterAction = 1002,
+    // ZoneServer
+    kZoneConrifmRequest = 1000,
+    kNotifyCurrentChrPosition = 1001,
+    kNotifyMonsterAction = 1002,
 
-	kInventoryUpdateRequest = 1003,
-	kQuickSlotUseRequest = 1004,
-	kEquipmentMountRequest = 1005,
+    kInventoryUpdateRequest = 1003,
+    kQuickSlotUseRequest = 1004,
+    kEquipmentMountRequest = 1005,
 
-	kMatchingRequest = 1006,
-	kMatchingConfirmRequest = 1007, // true / false
+    kMatchingRequest = 1006,
+    kMatchingConfirmRequest = 1007, // true / false
 
-	kCharacterAttack = 1008,
-	kCharcterHitSuccess = 1009,
+    kCharacterAttack = 1008,
+    kCharcterHitSuccess = 1009,
 
-	kMonsterHitCharacter = 1010,
-	kSendChatRequest = 1011,
+    kMonsterHitCharacter = 1010,
+    kSendChatRequest = 1011,
+
+    kRequestCharacterWait = 1020,
+    kRequestCharacterSprint = 1021,
+
 };
 
 
 enum class ENetworkSCOpcode : uint16_t
 {
-	// LoginServer
-	kCreateAccountResult = 0,
-	kLoginResult,
+    // LoginServer
+    kCreateAccountResult = 0,
+    kLoginResult,
 
-	// LobbyServer
-	kConfirmFailNotify = 100,
-	kCharacterCreateNotify,
-	kCharacterDeleteNotify,
-	kCharacterListNotify,
-	kMigrateZoneNotify,
+    // LobbyServer
+    kConfirmFailNotify = 100,
+    kCharacterCreateNotify,
+    kCharacterDeleteNotify,
+    kCharacterListNotify,
+    kMigrateZoneNotify,
 
-	// ZoneServer
-	kUserEnterTheMap = 1000,
-	kSpawnCharacter = 1002,
-	kSetMonsterController = 1003,
+    // ZoneServer
+    kUserEnterTheMap = 1000,
+    kSpawnCharacter = 1002,
+    kSetMonsterController = 1003,
 
-	kNotifyCharacterAttack = 1004,
-	kNotifyCharacterRolling = 1005,
-	kActorDamaged = 1006,
+    kNotifyCharacterAttack = 1004,
+    kNotifyCharacterRolling = 1005,
+    kActorDamaged = 1006,
 
-	kExitCharacter = 1007,
-	kUpdateCharacterPosition = 1008,
-	kUpdateMonsterAction = 1009,
+    kExitCharacter = 1007,
+    kUpdateCharacterPosition = 1008,
+    kUpdateMonsterAction = 1009,
 
-	kSpawnMonster = 1010,
+    kSpawnMonster = 1010,
 
-	kInventoryUpdate = 1011,
-	kQuickSlotUpdate = 1012,
+    kInventoryUpdate = 1011,
+    kQuickSlotUpdate = 1012,
 
-	kNotifyMatchResult = 1013,
-	kNotifyMatchCanceled = 1014,
-	kNotifyChat = 1015,
+    kNotifyMatchResult = 1013,
+    kNotifyMatchCanceled = 1014,
+    kNotifyChat = 1015,
+
+    kNotifyCharacterWait = 1020,
+    kNotifyCharacterSprint = 1021,
 };
