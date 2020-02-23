8092cfee828a8edd6d385eb3eb29bc4f7d7cd66b
kimminjin <skmj0617@naver.com>
2020-02-23 17:35:21 +0900

    빌드.cs수정

--- Source/XRProject/XRProject.Build.cs
+++ Source/XRProject/XRProject.Build.cs
@@ -9,20 +9,10 @@ public class XRProject : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject",
-            "InputCore", "AIModule", "NavigationSystem", "Slate", "SlateCore" ,"GameplayTasks"});
+            "InputCore", "AIModule", "NavigationSystem", "GameplayTasks"});
 
-<<<<<<< HEAD
-		if (Target.Type == TargetRules.TargetType.Editor)PrivateDependencyModuleNames.AddRange(new string[] { "Engine", "UMG" });
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-=======
-		PrivateDependencyModuleNames.AddRange(new string[] {
-              });
->>>>>>> master
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        if (Target.Type == TargetRules.TargetType.Editor) PrivateDependencyModuleNames.AddRange(new string[] { "Engine", "UMG", "Slate", "SlateCore" });
     }
 }
