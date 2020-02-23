4e9461356da09ecea57f21252b4ff84c61b93e00
kimminjin <skmj0617@naver.com>
2020-02-23 17:26:10 +0900

    미니맵 버그 수정

--- Source/XRProject/XRProject.Build.cs
+++ Source/XRProject/XRProject.Build.cs
@@ -9,16 +9,15 @@ public class XRProject : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject",
-            "Engine", "InputCore", "UMG", "AIModule", "NavigationSystem", "Slate", "SlateCore" ,"GameplayTasks"});
+            "InputCore", "AIModule", "NavigationSystem", "Slate", "SlateCore" ,"GameplayTasks"});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		if (Target.Type == TargetRules.TargetType.Editor)PrivateDependencyModuleNames.AddRange(new string[] { "Engine", "UMG" });
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
